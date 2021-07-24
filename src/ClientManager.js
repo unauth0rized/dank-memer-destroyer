7d6b4a2bc33313454b9bccc28a498b9e7ddef6a6
null-object-a <sr.pixelarmy@gmail.com>
2021-07-24 19:12:40 +0200

    Development session 1

--- src/ClientManager.js
+++ src/ClientManager.js
@@ -5,6 +5,12 @@ const ManagerBot = require('./ManagerBot')
 const CMDManager = null;
 const Parser = require("./TriviaParser")
 const logger = require('./utility/logger')
+Array.prototype.chunks = function (n) {
+  let items = [...this];
+  return new Array(Math.ceil(items.length / n))
+    .fill()
+    .map(() => items.splice(0, n));
+}
 
 class ClientManager {
   /**
@@ -26,18 +32,26 @@ class ClientManager {
     this.Workers = []
     this.tokens = [...new Set(tokens)]
   }
+  async ActiveHours(ammount = 3) {
+    const PossibleActiveHours = [0,1,2,3,4,5,6,7,8,9,10,13,14,15,16,17,18,19,20,21,22,23]
+    let chunks = PossibleActiveHours.chunks(ammount)
+    let chunk = chunks[Math.floor(Math.random() * chunks.length)]
+    if (chunks.length > ammount) {
+      chunk = chunk.splice(0, ammount)
+    }
+    logger.debug(`chunk\t${chunk}`)
+    return chunks
+  }
   async SetupWorkership() {
     this.ManagerToken = this.tokens[0]
     this.tokens.forEach(async (token) => {
       if (token == this.ManagerToken) { return; }
       let Worker = new MoneyCollector(token, this.activeHours, this.guildId, this.ownerId)
       this.Workers.push(Worker)
-      Worker.filter = null
       const { client } = Worker
       client.logger = logger
       Worker.SetHandler('ready', async () => {
-        //global.hook.send(client.user.tag + ' \'Ready\' event emitted.')
-       // client.user.setUsername(client.user.id)
+
         Worker.client.logger.ready(client.user.tag + ' is ready.')
         if (client.guilds.cache.get(this.guildId) == undefined) {
           console.log(client.user.tag, "Not in server.")
@@ -56,9 +70,7 @@ class ClientManager {
       Worker.SetHandler('message', async (message) => {
         if (Worker.cooldown) return;
         if (message.author.bot && message.embeds.length > 0 && message.embeds[0].description && message.embeds[0].description.includes("to answer with the correct letter") && message.embeds[0].author.name.includes(Worker.client.user.username) && message.channel.id === Worker.channel.id) {
-          var question = await Parser.get_question(message.embeds[0])
-          var answer = await Parser.get_answer(question, message)
-          message.channel.send(answer)
+          
         }
 
 
@@ -67,9 +79,13 @@ class ClientManager {
       setInterval(async () => {
         try {
           if (Worker.cooldown) return;
+          let filter = message => message.author.bot && message.embeds.length > 0 && message.embeds[0].description && message.embeds[0].description.includes("to answer with the correct letter") && message.embeds[0].author.name.includes(Worker.client.user.username) && message.channel.id === Worker.channel.id
           Worker.channel.send(['pls triv', 'pls trivia'][Math.floor(Math.random() * 2)])
           Worker.client.sweepMessages(30)
-          const message = Worker.channel.awaitMessages(Worker.filter, { max: 1, time: 10000, errors: ['time'] }).catch(Worker.client.logger.error).then(m => m.first())
+          const message = await Worker.channel.awaitMessages(filter,  { max: 1, time: 10000, errors: ['time'] }).catch(Worker.client.logger.error).then(m => m.first()).catch(Worker.client.logger.error)
+          var question = await Parser.get_question(message.embeds[0])
+          var answer = await Parser.get_answer(question, message)
+          message.channel.send(answer)
         }
         catch {
 
